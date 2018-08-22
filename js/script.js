89b0dbb6ca0b8370ce3cfd73e5e1b33271adc7ae
VolodymyrRomanytsia <romanyca@gmail.com>
2018-08-23 00:09:57 +0300

    Fixed JS

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-const button = document.querySelector('.button');
-const cont = document.querySelector('.container');
-button.onclick = function(){
+const rev = () => {
 	cont.classList.toggle('revers');
 	uah.value = 'null';
 	usd.value = 'null';
 }
+const button = document.querySelector('.button');
+const cont = document.querySelector('.container');
+button.addEventListener('click', rev); 
+
 
 function exchange() {
 	const exchangeRate = 26;
-	let ua = document.getElementById('uah').value;
-	let us = document.getElementById('usd').value;
-	if (uah.value.length > 0 && usd.value.length > 0) {
-		uah.value = 'null';
-		usd.value = 'null';
-	} else if (uah.value.length > 0) {
+	const ua = document.getElementById('uah').value;
+	const us = document.getElementById('usd').value;
+	if (ua != 0 && us != 0) {
+		ua = 'null';
+		us = 'null';
+	} else if (ua > 0) {
 		document.getElementById('usd').value = ua / exchangeRate;
 		document.getElementById('uah').value = ua;
-	} else if (usd.value.length > 0) {	
+	} else if (us > 0) {	
 		document.getElementById('uah').value = us * exchangeRate;
 		document.getElementById('usd').value = us;
 	} 
